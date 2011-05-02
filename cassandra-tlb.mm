09cd2931d785e7da1ee8f50aaab24d97f7996789
Janmejay Singh <singh.janmejay@gmail.com>
2011-05-02 06:54:39 +0530

    got cassandra server working + added a mind-map of data-structuring in cassandra

---
+++ cassandra-tlb.mm
@@ -0,0 +1,17 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1304298350012" ID="Freemind_Link_1738859008" MODIFIED="1304298596892" TEXT="keyspace: tlb">
+<node CREATED="1304298446537" ID="_" MODIFIED="1304298985049" POSITION="right" STYLE="bubble" TEXT="super-column-family: jobs">
+<node CREATED="1304298469359" ID="Freemind_Link_533407242" MODIFIED="1304298486501" TEXT="key: job-namespace"/>
+<node CREATED="1304298510633" ID="Freemind_Link_140411904" MODIFIED="1304299083302" TEXT="sub-key: job-version"/>
+<node CREATED="1304298540248" ID="Freemind_Link_1547414894" MODIFIED="1304299117321" TEXT="sub-columns: test-data-id, subset-size etc"/>
+<node CREATED="1304299128365" ID="Freemind_Link_625403484" MODIFIED="1304299157646" TEXT="sub-values: UUID, integer etc"/>
+</node>
+<node CREATED="1304298567938" ID="Freemind_Link_1817409044" MODIFIED="1304299316542" POSITION="left" STYLE="bubble" TEXT="super-column-family: testData">
+<node CREATED="1304298614740" ID="Freemind_Link_580476753" MODIFIED="1304298654192" TEXT="key: UUID(A)"/>
+<node CREATED="1304298644995" ID="Freemind_Link_984438031" MODIFIED="1304298691489" TEXT="sub-key: test-case-name"/>
+<node CREATED="1304298692987" ID="Freemind_Link_1301834051" MODIFIED="1304298726253" TEXT="sub-value: test-data-values(time, result etc)"/>
+<node CREATED="1304298727705" ID="Freemind_Link_271198018" MODIFIED="1304298778293" TEXT="sub-columns: time, result, checksum(future) etc"/>
+</node>
+</node>
+</map>
